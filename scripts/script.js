3a18cc8223a9160a2fa5084faaca14fb1233f257
Robert <treboryuzaky@gmail.com>
2022-03-29 12:10:42 -0400

    contabilidad, administración y discord

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 //Diccionario de links
-var diccio = {"proyecto1": "https://chat.whatsapp.com/DETUrqVaJyw9wVVXItwDR2", "AdminYConta":"https://chat.whatsapp.com/Eqrtz2F3hYkBUEPPp0g1CD"};
+var diccio = {"proyecto1": "https://chat.whatsapp.com/DETUrqVaJyw9wVVXItwDR2", "AdminYConta":"https://chat.whatsapp.com/LPbWEEFsqET0XjwCtZ8iO4"};
 
 //abrir enlaces de whatsapp
 function enlaces(clave) {

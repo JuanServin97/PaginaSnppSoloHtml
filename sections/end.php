57d949132d6087a5cea38c3f920ae7749ebf567a
Robert Romero <80548396+RobertR03@users.noreply.github.com>
2022-09-02 11:22:02 -0400

    borro codigo php inutilizable

--- sections/end.php
+++
@@ -1,9 +0,0 @@
-        <footer class="py-5 mt-5 text-muted text-center text-small">
-            <p class="mb-1">Robert Romero</p>
-            <ul class="list-inline">
-            <li class="list-inline-item"><a href="https://github.com/RobertR03/" target="_blank">GitHub</a></li>
-            <li class="list-inline-item"><a href="https://wa.me/595984748295?text=Hola%20Robert!" target="_blank">WhatsApp</a></li>
-            </ul>
-        </footer>
-    </body>
-</html>
